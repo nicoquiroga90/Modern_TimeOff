681fe74e7e8fa226c0afcdac929cb120d15eb165
Nico <akebra77@gmail.com>
2024-07-09 07:37:02 +0200

    fixed bug in dashboard

--- components/Functionalities/Loading.jsx
+++ components/Functionalities/Loading.jsx
@@ -1,22 +1,29 @@
-"use client"
+"use client";
 
+import { useEffect, useState } from "react";
 import Dialog from "@mui/material/Dialog";
 import DialogContent from "@mui/material/DialogContent";
 import { leapfrog } from "ldrs";
 
 leapfrog.register();
-function Loading({ open }) { 
+
+function Loading({ open }) {
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  if (!isClient) {
+    return null;
+  }
 
   return (
-      <Dialog
-        open={open}
-        aria-describedby="alert-dialog-description"
-      >
-        <DialogContent>
-            <l-leapfrog size="50" speed="2.5" color="#47a67e"  bg-opacity=".01"></l-leapfrog>
-         
-        </DialogContent>
-      </Dialog>
+    <Dialog open={open} aria-describedby="alert-dialog-description">
+      <DialogContent>
+        <l-leapfrog size="50" speed="2.5" color="#47a67e" bg-opacity=".01"></l-leapfrog>
+      </DialogContent>
+    </Dialog>
   );
 }
 
