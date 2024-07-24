d93baa5bb21557c687ba5a757c82ccdd796c0608
Nico <akebra77@gmail.com>
2024-07-24 20:36:31 +0200

    added icon

--- next.config.js
+++ next.config.js
@@ -4,8 +4,11 @@ const nextConfig = {
   reactStrictMode: false,
   swcMinify: true,
   images: {
-    domains: ["localhost"],
     remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "localhost",
+      },
       {
         protocol: "https",
         hostname: "cdn.sanity.io",
