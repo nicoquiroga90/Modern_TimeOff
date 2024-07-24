0f8c0617dfafa8512e747e6deb06ca4b945f7c7e
Nico <akebra77@gmail.com>
2024-07-24 21:57:04 +0200

    Setup CORS

--- next.config.js
+++ next.config.js
@@ -16,6 +16,19 @@ const nextConfig = {
       },
     ],
   },
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "https://modern-time-off.vercel.app/" }, 
+          { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "Content-Type, Authorization" },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
