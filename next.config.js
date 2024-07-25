6b37a721a7a51a41edd2f7ae6e540cc647ef7b52
Nico <akebra77@gmail.com>
2024-07-25 09:01:24 +0200

    cors mobile

--- next.config.js
+++ next.config.js
@@ -19,13 +19,23 @@ const nextConfig = {
   async headers() {
     return [
       {
+        // Aplica los encabezados CORS a todas las rutas en /api/
         source: "/api/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" }, 
+          { key: "Access-Control-Allow-Origin", value: "*" },
           { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
           { key: "Access-Control-Allow-Headers", value: "Content-Type, Authorization" },
         ],
+      },
+    ];
+  },
+  async rewrites() {
+    return [
+      {
+        // Reescribe todas las rutas en /api/ a tu servidor
+        source: "/api/:path*",
+        destination: "https://modern-time-off.vercel.app/:path*", // Nota: removido el doble slash
       },
     ];
   },
