2de907e581cf4bfbf9b3285f6d8112c5058b996e
Nico <akebra77@gmail.com>
2024-07-31 09:17:44 +0200

    Added Testimonial

--- components/Testimonial/index.tsx
+++ components/Testimonial/index.tsx
@@ -22,7 +22,7 @@ const Testimonial = () => {
               headerInfo={{
                 title: `TESTIMONIALS`,
                 subtitle: `Client’s Testimonials`,
-                description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. In convallis tortor eros. Donec vitae tortor lacus. Phasellus aliquam ante in maximus.`,
+                description: `Our clients love our work. Read what they say about our service.`,
               }}
             />
           </div>
